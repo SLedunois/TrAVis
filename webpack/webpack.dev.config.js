029bd4685bda32c7bc4eaa1171f279974f8bdbd2
LEDUNOIS Simon <simon.ledunois@gmail.com>
2018-01-19 12:24:21 +0100

    Adding eslint configuration

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,37 +1,36 @@
-const webpack = require('webpack');
 const path = require('path');
 const Jarvis = require('webpack-jarvis');
 
 const parentDir = path.join(__dirname, '../');
 
 module.exports = {
-    entry: [
-        path.join(parentDir, 'app/App.js')
+  entry: [
+    path.join(parentDir, 'app/App.js'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                loaders: ['style-loader', 'css-loader']
-            }
-        ],
-    },
-    plugins: [
-        new Jarvis({
-            port: 1337
-        })
-    ],
-    output: {
-        path: parentDir + 'dist',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: parentDir + 'dist',
-        historyApiFallback: true
-    }
-}
+  },
+  plugins: [
+    new Jarvis({
+      port: 1337,
+    }),
+  ],
+  output: {
+    path: `${parentDir}dist`,
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: `${parentDir}dist`,
+    historyApiFallback: true,
+  },
+};
