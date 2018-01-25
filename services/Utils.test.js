c6e47b66f1071b9fdc5b5f36e3273333154812e7
LEDUNOIS Simon <simon.ledunois@gmail.com>
2018-01-25 15:17:45 +0100

    Adding test to Utils service

---
+++ services/Utils.test.js
@@ -0,0 +1,7 @@
+const Utils = require('./Utils');
+
+test('[UtilsService@validPassword] It should valid the password', async () => {
+  const password = 'password-test';
+  expect(Utils.validPassword(password, Utils.generateHash(password)))
+    .toBe(true);
+});
