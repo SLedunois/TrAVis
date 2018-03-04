5cc1602636b90ab843de3a0d05516a41f4cc4245
LEDUNOIS Simon <simon.ledunois@gmail.com>
2018-03-04 17:24:07 +0100

    Update style colors

--- app/icons/Logo.jsx
+++ app/icons/Logo.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Logo = () => (
   <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <rect id="Rectangle" fill="#5276FF" transform="translate(12.500000, 19.432263) rotate(180.000000) translate(-12.500000, -19.432263) " x="9.7826087" y="4.97379915e-14" width="5.43478261" height="38.8645269" rx="0.8" />
-    <rect id="Rectangle" fill="#5276FF" transform="translate(22.282609, 33.780639) rotate(180.000000) translate(-22.282609, -33.780639) " x="19.5652174" y="28.8019152" width="5.43478261" height="9.95744681" rx="0.8" />
-    <rect id="Rectangle" fill="#45DBBE" transform="translate(22.282609, 11.617021) rotate(180.000000) translate(-22.282609, -11.617021) " x="19.5652174" y="6.63829787" width="5.43478261" height="9.95744681" rx="0.8" />
-    <rect id="Rectangle" fill="#5276FF" transform="translate(2.717391, 4.978723) rotate(180.000000) translate(-2.717391, -4.978723) " x="2.84217094e-14" y="-1.77635684e-14" width="5.43478261" height="9.95744681" rx="0.8" />
-    <rect id="Rectangle" fill="#45DBBE" transform="translate(2.717391, 24.893617) rotate(180.000000) translate(-2.717391, -24.893617) " x="3.19744231e-14" y="19.9148936" width="5.43478261" height="9.95744681" rx="0.8" />
-    <ellipse id="Oval" fill="#FFD00E" transform="translate(2.717391, 14.936170) rotate(180.000000) translate(-2.717391, -14.936170) " cx="2.7173913" cy="14.9361702" rx="2.7173913" ry="2.76595745" />
-    <ellipse id="Oval" fill="#FFD00E" transform="translate(22.282609, 22.680851) rotate(180.000000) translate(-22.282609, -22.680851) " cx="22.2826087" cy="22.6808511" rx="2.7173913" ry="2.76595745" />
+    <rect id="Rectangle" fill="#655FFF" transform="translate(12.500000, 19.432263) rotate(180.000000) translate(-12.500000, -19.432263) " x="9.7826087" y="4.97379915e-14" width="5.43478261" height="38.8645269" rx="0.8" />
+    <rect id="Rectangle" fill="#655FFF" transform="translate(22.282609, 33.780639) rotate(180.000000) translate(-22.282609, -33.780639) " x="19.5652174" y="28.8019152" width="5.43478261" height="9.95744681" rx="0.8" />
+    <rect id="Rectangle" fill="#5773FF" transform="translate(22.282609, 11.617021) rotate(180.000000) translate(-22.282609, -11.617021) " x="19.5652174" y="6.63829787" width="5.43478261" height="9.95744681" rx="0.8" />
+    <rect id="Rectangle" fill="#655FFF" transform="translate(2.717391, 4.978723) rotate(180.000000) translate(-2.717391, -4.978723) " x="2.84217094e-14" y="-1.77635684e-14" width="5.43478261" height="9.95744681" rx="0.8" />
+    <rect id="Rectangle" fill="#5773FF" transform="translate(2.717391, 24.893617) rotate(180.000000) translate(-2.717391, -24.893617) " x="3.19744231e-14" y="19.9148936" width="5.43478261" height="9.95744681" rx="0.8" />
+    <ellipse id="Oval" fill="#FF9057" transform="translate(2.717391, 14.936170) rotate(180.000000) translate(-2.717391, -14.936170) " cx="2.7173913" cy="14.9361702" rx="2.7173913" ry="2.76595745" />
+    <ellipse id="Oval" fill="#FF9057" transform="translate(22.282609, 22.680851) rotate(180.000000) translate(-22.282609, -22.680851) " cx="22.2826087" cy="22.6808511" rx="2.7173913" ry="2.76595745" />
   </svg>
 );
 
