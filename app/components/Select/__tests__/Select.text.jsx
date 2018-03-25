1a4d26a156312a7b1764cfafb5bf40fedb0e3beb
LEDUNOIS Simon <simon.ledunois@gmail.com>
2018-03-25 14:14:37 +0200

    Init Select test component

---
+++ app/components/Select/__tests__/Select.text.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MemoryRouter, NavLink } from 'react-router-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Select from '../Select';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('[Component@Select]', () => {
+  it('should render without throwing and error', () => {
+    const select = shallow(<Select />);
+    expect(select.exists()).toBe(true);
+  });
+});
